282be38782c2d64e1b1952f27cff425be004a453
djrm11 <djrm11@qq.com>
2019-08-21 23:35:16 +0800

    commit

--- src/main/java/com/pazlbackup/controller/LogbackupController.java
+++ src/main/java/com/pazlbackup/controller/LogbackupController.java
@@ -1,18 +1,20 @@
 package com.pazlbackup.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogbackupController {
-    private final static Logger logger = LoggerFactory.getLogger(LogbackupController.class);
+//    private final static Logger logger = LoggerFactory.getLogger(LogbackupController.class);
 
     @RequestMapping("/logbackup/eg")
     String home(){
-        logger.info("logback 访问hello");
-        logger.error("logback 访问hello");
+        log.info("logback 访问hello");
+        log.error("logback 访问hello");
         return "Hello World!";
     }
 
