14bdbcdf007a6934f75910e8a49eab831003e302
unknown <djrm11@qq.com>
2020-03-17 09:49:26 +0800

    add @ServletComponentScan

--- springboot2security/README.md
+++ springboot2security/README.md
@@ -4,4 +4,4 @@ springboot2.0
 mybatis
 logback
 spring security
-Druid
+Druid   https://blog.csdn.net/weixin_42040802/article/details/84284183
--- springboot2security/src/main/java/com/jafe/springboot2/Springboot2SecurityApp.java
+++ springboot2security/src/main/java/com/jafe/springboot2/Springboot2SecurityApp.java
@@ -2,10 +2,11 @@ package com.jafe.springboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-
+@ServletComponentScan
 @SpringBootApplication
 public class Springboot2SecurityApp {
 
