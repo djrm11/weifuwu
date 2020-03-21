7fab1be48b748c9fd91e59ccc5dce4e0b6205737
unknown <djrm11@qq.com>
2020-03-21 17:19:19 +0800

    commit

--- springboot2security/src/main/java/com/jafe/springboot2/configuration/DruidStatFilter.java
+++ springboot2security/src/main/java/com/jafe/springboot2/configuration/DruidStatFilter.java
@@ -1,20 +1,20 @@
-package com.jafe.springboot2.configuration;
-
-
-import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
-
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.annotation.WebServlet;
-
-/**
- * druid过滤器.
- */
-@WebFilter(filterName="druidWebStatFilter",
-        urlPatterns="/*",
-        initParams={
-                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")//忽略资源
-        }
-)
-public class DruidStatFilter  extends DruidStatProperties.WebStatFilter {
-}
+//package com.jafe.springboot2.configuration;
+//
+//
+//import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+//
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.annotation.WebInitParam;
+//import javax.servlet.annotation.WebServlet;
+//
+///**
+// * druid过滤器.
+// */
+//@WebFilter(filterName="druidWebStatFilter",
+//        urlPatterns="/*",
+//        initParams={
+//                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")//忽略资源
+//        }
+//)
+//public class DruidStatFilter  extends DruidStatProperties.WebStatFilter {
+//}
